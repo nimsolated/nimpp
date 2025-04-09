4ec896204deca48b752d9cef5fdadb86537b981f
nimsolated <armindoosti03@gmail.com>
2025-04-09 02:01:17 -0700

    commit from ubuntu. out build.

---
+++ out/build/GCC 13.3.0 x86_64-linux-gnu/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/nimsolated/Desktop/Repos/nimpp
+# Build directory: /home/nimsolated/Desktop/Repos/nimpp/out/build/GCC 13.3.0 x86_64-linux-gnu
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
